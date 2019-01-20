d7b29933cd82cb8889272973a49a7693a2842b15
KIM <stringee@gmail.com>
2019-01-20 20:57:36 +0100

    Provider in index.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React, { Component } from "react";
 
-import { Provider } from "react-redux";
-
 import Posts from "./components/Posts";
 import Postform from "./components/Postform";
 
-import store from "./store";
 class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <div>
-          <Postform />
-          <hr />
-          <Posts />
-        </div>
-      </Provider>
+      <div>
+        <Postform />
+        <hr />
+        <Posts />
+      </div>
     );
   }
 }
